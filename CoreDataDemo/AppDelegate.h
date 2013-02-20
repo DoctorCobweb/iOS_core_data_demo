8c2ac6ea37d79b29f2bf93229135fe94438ab7e5
andre trosky <andre@andre-mbp.bigpond>
2013-02-20 15:23:51 +1100

    Initial Commit

---
+++ CoreDataDemo/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  CoreDataDemo
+//
+//  Created by andre trosky on 20/02/13.
+//  Copyright (c) 2013 andre trosky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
